20e3cb3fb5a8bd328f85b04098398ffaee1b2508
davistheweb <josiahdave001@gmail.com>
2025-09-18 16:49:58 +0100

    model and context update

--- utils/chat.py
+++ utils/chat.py
@@ -19,7 +19,7 @@ def Klosanow_Chat(prompt = '') ->str:
     	# {'role' : 'model', 'text' : "That's an interesting question because \"Python\" can refer to two very different things!.  **The Animal:** A **python** is a type of large, non-venomous constrictor snake native to Africa, Asia, and Australia. They kill their prey by coiling around them and suffocating them (constriction) rather than using venom. There are many different species, including the reticulated python (one of the longest snakes in the world), the ball python (a popular pet), and the Burmese python. **The Programming Language:** **Python** is also a very popular and powerful high-level **programming language**. It's known for its readability, versatility, and extensive libraries. It's widely used for:*   Web development (e.g., Django, Flask) *   Data science and analysis *   Artificial intelligence and machine learning*   Automation and scripting *   Scientific computing*   Game development **Which one were you curious about?** Let me know, and I can tell you more!"}
 		# ]
         
-        response = client.models.generate_content( model="gemini-2.5-flash", contents=f"{RESPONSIBILITIES} \n User_Chat:{prompt}", )
+        response = client.models.generate_content( model="gemini-2.0-flash", contents=f"{RESPONSIBILITIES} \n User_Chat:{prompt}", )
         
         return response.text
     except Exception as e:
